46a0010d0b7d0a62fdad132214daf86b0880202d
Grimanus <filipbui12@gmail.com>
2023-02-28 09:02:33 +0100

    .

---
+++ src/components/Bottom.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div id="bottom"></div>
+</template>
+
+<style>
+    #bottom
+    {
+        border-width: 3.571px;
+        border-color: rgb(0, 0, 0);
+        border-style: solid;
+        border-radius: 50%;
+        background-color: rgb(255, 255, 255);
+        position: absolute;
+        left: 44px;
+        top: 223px;
+        width: 155.858px;
+        height: 140.858px;
+        z-index: 1;
+    }
+</style>
---
+++ src/components/Hlava.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div id="hlava"></div>
+</template>
+
+<style>
+    #hlava
+    {
+        border-width: 3px;
+        border-color: rgb(0, 0, 0);
+        border-style: solid;
+        border-radius: 50%;
+        background-color: rgb(255, 255, 255);
+        position: absolute;
+        left: 81px;
+        top: 67px;
+        width: 74px;
+        height: 76px;
+        z-index: 3;
+    }
+</style>
